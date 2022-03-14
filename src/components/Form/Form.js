fd7628b864f84f7e5543786d81e88818a54db24c
Daniel-17 <frimudaniel17@gmail.com>
2022-03-14 19:02:07 +0100

    Added input data

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,32 +1,39 @@
 import React from "react";
+import { inputData } from "./InputData";
 import classes from "./Form.module.css";
 
 const Form = () => {
   return (
     <section style={{ paddingTop: "20px" }}>
       <form className="d-flex flex-wrap">
-        <div className="col-sm-6 d-flex align-items-center mb-3">
-          <label
-            htmlFor="input"
-            style={{ width: "30%", textAlign: "right", fontWeight: "bold" }}
-          >
-            Execution process start date from
-          </label>
-
-          <div className="w-50 mx-4 position-relative">
-            <input
-              type="date"
-              id="input"
-              className={`${classes["form-control"]} w-100 px-2`}
-            />
-          </div>
-          <div style={{ width: "20%" }}>
-            <input type="checkbox" id="remember" />
-            <label htmlFor="remember" className="mx-2 fw-bold">
-              Reverse
+        {inputData.map((input) => (
+          <div className="col-sm-6 d-flex align-items-center mb-3">
+            <label
+              htmlFor={input.htmlFor}
+              style={{ width: "30%", textAlign: "right", fontWeight: "bold" }}
+            >
+              {input.label}
             </label>
+
+            <div className="w-50 mx-4 position-relative">
+              <input
+                type={input.inputType}
+                id={input.htmlFor}
+                className={`${classes["form-control"]} w-100 px-2`}
+              />
+            </div>
+            <div style={{ width: "20%" }}>
+              {input.hasCheckbox && (
+                <div>
+                  <input type="checkbox" id="remember" />
+                  <label htmlFor="remember" className="mx-2 fw-bold">
+                    Reverse
+                  </label>
+                </div>
+              )}
+            </div>
           </div>
-        </div>
+        ))}
       </form>
     </section>
   );
---
+++ src/components/Form/InputData.js
@@ -0,0 +1,23 @@
+export const inputData = [
+  {
+    id: 1,
+    label: "Client contract",
+    htmlFor: "client-contract",
+    inputType: "text",
+    hasCheckbox: true,
+  },
+  {
+    id: 2,
+    label: "Client",
+    htmlFor: "client",
+    inputType: "text",
+    hasCheckbox: true,
+  },
+  {
+    id: 3,
+    label: "Claim stage",
+    htmlFor: "claim-stage",
+    inputType: "text",
+    hasCheckbox: false,
+  },
+];
