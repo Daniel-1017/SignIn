787929a84d3b7e0203b9d8d69dc4cad019e17e73
Daniel-17 <frimudaniel17@gmail.com>
2022-03-14 18:40:30 +0100

    Best form ever created

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,23 +1,33 @@
 import React from "react";
 import classes from "./Form.module.css";
-import Container from "../Container/Container";
 
 const Form = () => {
   return (
-    <section>
-      <Container className={classes.container}>
-        <form className="w-100 d-flex justify-content-between flex-wrap">
-          <div className="col-md-5 d-flex align-items-center mb-2">
-            <label htmlFor="inputEmail4">Email</label>
+    <section style={{ paddingTop: "20px" }}>
+      <form className="d-flex flex-wrap">
+        <div className="col-sm-6 d-flex align-items-center mb-3">
+          <label
+            htmlFor="input"
+            style={{ width: "30%", textAlign: "right", fontWeight: "bold" }}
+          >
+            Execution process start date from
+          </label>
+
+          <div className="w-50 mx-4 position-relative">
             <input
-              type="email"
-              className={`form-control ${classes.input}`}
-              id="inputEmail4"
-              placeholder="Email"
+              type="date"
+              id="input"
+              className={`${classes["form-control"]} w-100 px-2`}
             />
           </div>
-        </form>
-      </Container>
+          <div style={{ width: "20%" }}>
+            <input type="checkbox" id="remember" />
+            <label htmlFor="remember" className="mx-2 fw-bold">
+              Reverse
+            </label>
+          </div>
+        </div>
+      </form>
     </section>
   );
 };
